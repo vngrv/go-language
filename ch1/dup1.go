cb69d3c4fc9b6972cd3ba49fee987865f2e8b253
Dmitry Vengerov <hardbeat34@gmail.com>
2021-01-19 12:39:38 +0300

    add dup1.go and dup2.go

--- ch1/dup1.go
+++ ch1/dup1.go
@@ -12,11 +12,12 @@ func main() {
 
 	for input.Scan() {
 		counts[input.Text()]++
-	}
 
-	for line, n := range counts {
-		if n > 1 {
-			fmt.Println("%d\t%s\n", n, line)
+		for line, n := range counts {
+			if n > 1 {
+				fmt.Printf("%d\t%s\n", n, line)
+			}
 		}
 	}
+
 }
---
+++ ch1/dup2.go
@@ -0,0 +1,16 @@
+package main
+
+import "os"
+
+func main() {
+	counts := make(map[string]int)
+	files := os.Args[1:]
+
+	if len(files) === 0 {
+		
+	}
+}
+
+func countLines() {
+	
+}
